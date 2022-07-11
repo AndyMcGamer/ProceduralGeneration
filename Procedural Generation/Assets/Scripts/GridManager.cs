5f5240983fa7c7dae9eae77aa369a56752fd5bf3
AndyMcGamer <andymcgamer@gmail.com>
2022-07-11 01:10:42 -0700

    Starting the process

---
+++ Procedural Generation/Assets/Scripts/GridManager.cs
@@ -0,0 +1,17 @@
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class GridManager : MonoBehaviour
+{
+    // Generate Vertices In Hexagon
+
+    // Delaney Triangulation or something (make triangles)
+
+    // Combine some number of triangles into quadrilaterals
+
+    // Subdivide quads and triangles
+        // Make center vertex, draw segments from midpoints of edges to center
+
+    // Skew vertices somehow (figure this one out or ask Oskar)
+}
